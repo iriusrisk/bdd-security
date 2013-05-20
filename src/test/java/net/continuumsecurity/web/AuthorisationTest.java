b4a40a763a1e02539e2a0a027aa8b35b5b894aa1
karlosmid <karlo.smid@gmail.com>
2013-05-20 17:02:00 +0200

    finished authorisation ng tests

--- src/test/java/net/continuumsecurity/web/AuthorisationTest.java
+++ src/test/java/net/continuumsecurity/web/AuthorisationTest.java
@@ -39,5 +39,27 @@ public class AuthorisationTest {
       webAppSteps.checkAccessToResource((String)item.get("verifyString"),(String)item.get("method"));
     }
   }
+
+  @Test
+  public void users_must_not_be_able_to_view_resources_for_which_they_are_not_authorised{
+    for(HashMap item: workingDirectory+"/src/main/stories/tables/unauthorised.resources.table"){
+      webAppSteps.createApp();
+      webAppSteps.openLoginPage();
+      webAppSteps.setUsernameFromExamples((String)item.get("username"));
+      webAppSteps.setCredentialsFromExamples((String)item.get("password"));
+      webAppSteps.loginWithSetCredentials();
+      webAppSteps.checkNoAccessToResource((String)item.get("verifyString"),(String)item.get("method"));
+    }
+  }
+
+  @Test
+  public void un-authenticated_users_should_not_be_able_to_view_restricted_resources{
+    for(HashMap item: this.exampleTable){
+      webAppSteps.checkIfMapPopulated();
+      webAppSteps.createApp();
+      webAppSteps.openLoginPage();
+      webAppSteps.checkNoAccessToResource((String)item.get("verifyString"),(String)item.get("method"));
+    }
+  }
 }
 
