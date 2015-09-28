f8ffa5af21dc31621e714c0a04443d51bf70fcd0
continuumsecurity <stephen@continuumsecurity.net>
2015-09-28 08:10:22 +0200

    Reference to PortResult changed to Port in tests

--- src/test/java/net/continuumsecurity/internal/scanner/PortScannerTest.java
+++ src/test/java/net/continuumsecurity/internal/scanner/PortScannerTest.java
@@ -1,6 +1,6 @@
 package net.continuumsecurity.internal.scanner;
 
-import net.continuumsecurity.scanner.PortResult;
+import net.continuumsecurity.Port;
 import net.continuumsecurity.scanner.PortScanner;
 import org.junit.Test;
 
@@ -13,29 +13,29 @@ public class PortScannerTest {
     @Test
     public void testPortOpen() throws ExecutionException, InterruptedException {
         scanner = new PortScanner("www.google.com",80,80,1,500);
-        List<PortResult> results = scanner.scan();
+        List<Port> results = scanner.scan();
         assert results.size() == 1;
-        assert results.get(0).getPort() == 80;
-        assert results.get(0).getState() == PortResult.PortState.OPEN;
+        assert results.get(0).getNumber() == 80;
+        assert results.get(0).getState() == Port.State.OPEN;
     }
 
 
     @Test
     public void testPortClosed() throws ExecutionException, InterruptedException {
         scanner = new PortScanner("127.0.0.1",65531,65531,1,500);
-        List<PortResult> results = scanner.scan();
+        List<Port> results = scanner.scan();
         assert results.size() == 1;
-        assert results.get(0).getPort() == 65531;
-        assert results.get(0).getState() == PortResult.PortState.CLOSED;
+        assert results.get(0).getNumber() == 65531;
+        assert results.get(0).getState() == Port.State.CLOSED;
     }
 
     @Test
     public void testPortTimedOut() throws ExecutionException, InterruptedException {
         scanner = new PortScanner("www.google.com",83,83,1,500);
-        List<PortResult> results = scanner.scan();
+        List<Port> results = scanner.scan();
         assert results.size() == 1;
-        assert results.get(0).getPort() == 83;
-        assert results.get(0).getState() == PortResult.PortState.TIMEDOUT;
+        assert results.get(0).getNumber() == 83;
+        assert results.get(0).getState() == Port.State.TIMEDOUT;
     }
 
 }
