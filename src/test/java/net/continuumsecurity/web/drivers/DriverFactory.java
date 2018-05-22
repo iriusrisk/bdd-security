d7580754bab46ccc28d046bbc2391347b12adba6
nicolasiltis <nicoiltis@estvideo.fr>
2018-05-22 14:36:21 +0200

    add htmlunit dependence
    Since htmlunit isn't in the selenium project we have to add it manually in the build.gradle.
    (https://github.com/SeleniumHQ/selenium/commit/05325a7c1ebf59e838120b789ab0739f48e50344#diff-1998ab834c50706eec2893da4561cc50)

--- src/test/java/net/continuumsecurity/web/drivers/DriverFactory.java
+++ src/test/java/net/continuumsecurity/web/drivers/DriverFactory.java
@@ -29,6 +29,7 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import java.io.File;
 
@@ -170,7 +171,8 @@ public class DriverFactory {
 	    capabilities = DesiredCapabilities.firefox();
 	    break;
 	case HTMLUNIT:
-	    capabilities = DesiredCapabilities.htmlunit();
+	    capabilities = DesiredCapabilities.htmlUnit();
+	    break;
 	default:
 	    break;
 	}
